f5346a418ed5935b38a34d83e29d0dc307943402
Shahid321fw11 <sidansari20@gmail.com>
2023-09-29 19:48:30 +0530

    Style: Change style for body background

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="container">
-         {/* Add title here in h1 tag */}
-      <div>
-        {/* Add BillPage component */}
-      </div>
+      {/* Add title here in h1 tag */}
+      <h1>SPLITTER</h1>
+      <div>{/* Add BillPage component */}</div>
     </div>
   );
 };
